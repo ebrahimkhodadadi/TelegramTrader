4c0992b658892d65f932708853a9fb61deabb6b6
EbrahimKhodadadi <ebrahimkhodadadideveloper@gmail.com>
2025-05-05 16:07:54 +0330

    update analayzer

--- app/Analayzer/Analayzer.py
+++ app/Analayzer/Analayzer.py
@@ -6,11 +6,13 @@ import re
 import os
 from MetaTrader import *
 
+
 def extract_price(message):
     match = re.search(r'@[\s]*([0-9]+(?:\.[0-9]+)?)', message)
     if match:
         return float(match.group(1))
     return None
+
 
 def parse_message(message):
     try:
@@ -25,24 +27,29 @@ def parse_message(message):
         firstPrice = GetFirstPrice(message)
         secondPrice = GetSecondPrice(message)
         takeProfits = GetTakeProfits(message)
-        stopLoss = GetStopLoss(message)        
+        stopLoss = GetStopLoss(message)
         symbol = GetSymbol(message)
 
         if firstPrice == secondPrice or secondPrice in takeProfits or secondPrice == stopLoss:
             secondPrice = None
-            
+
         return actionType, symbol, firstPrice, secondPrice, takeProfits, stopLoss
     except Exception as e:
         logger.error("Error while deserilize message: \n" + e)
         return None, None, None, None, None, None
 
+
 def clean_text(text):
     """Normalize text by removing only special Unicode formatting, keeping Persian and new lines."""
     text = unicodedata.normalize("NFKC", text)  # Normalize bold/italic Unicode
-    text = re.sub(r'[^\S\r\n]+', ' ', text)  # Remove excessive spaces but keep new lines
+    # Remove excessive spaces but keep new lines
+    text = re.sub(r'[^\S\r\n]+', ' ', text)
     text = re.sub(r'[☑️❌]', '', text)  # Remove ☑️ and ❌ symbols
     text = re.sub(r'[^\w\s.,:;!?(){}\[\]/\-+=@#%&*\'\"<>]', '', text)
+    superscript_map = str.maketrans("¹²³⁴⁵⁶⁷⁸⁹⁰", "1234567890")
+    text = text.translate(superscript_map)
     return text.strip()
+
 
 def GetFirstPrice(message):
     try:
@@ -60,19 +67,23 @@ def GetFirstPrice(message):
         #              message + "' for first price: \n" + e)
         return None
 
+
 def GetSecondPrice(message):
     try:
-        match = re.search(r'\b\d+\.?\d*///(\d+\.?\d*)', message)  # <--- New pattern added
+        match = re.search(r'\b\d+\.?\d*///(\d+\.?\d*)',
+                          message)  # <--- New pattern added
         if not match:
             match = re.search(r'@\d+\.?\d*\s*-\s*(\d+\.?\d*)', message)
         if not match:
-            match = re.search(r'2(?:nd)?\s+limit\s*@\s*(\d+\.?\d*)', message, re.IGNORECASE)
+            match = re.search(
+                r'2(?:nd)?\s+limit\s*@\s*(\d+\.?\d*)', message, re.IGNORECASE)
         if not match:
-            match = re.search(r'\b\d+\.?\d*__+(\d+\.?\d*)', message)     
+            match = re.search(r'\b\d+\.?\d*__+(\d+\.?\d*)', message)
         if not match:
             match = re.search(r'@\s*\d+\.?\d*\s*-\s*(\d+\.?\d*)', message)
         if not match:
-            match = re.search(r'@\s*\d+\.?\d*\s*-\s*(\d+\.?\d*)|:\s*\d+\.?\d*\s*-\s*(\d+\.?\d*)', message)
+            match = re.search(
+                r'@\s*\d+\.?\d*\s*-\s*(\d+\.?\d*)|:\s*\d+\.?\d*\s*-\s*(\d+\.?\d*)', message)
         if not match:
             match = re.search(r'\b\d+\.?\d*\s*-\s*(\d+\.?\d*)', message)
         if not match:
@@ -94,15 +105,18 @@ def GetSecondPrice(message):
     except Exception as e:
         return None
 
+
 def GetTakeProfits(message):
     try:
         tp_numbers = []
         sentences = re.split(r'\n+', message)
         for sentence in sentences:
-            words = re.findall(r'\b\d+\b|\btp\b', sentence.lower())
             # Extract TP numbers for various formats
             tp_match = re.findall(
-                r'tp\s*(?:\d*\s*:\s*)?(\d+\.\d+)', sentence, re.IGNORECASE)
+                r'tp\s*\d*\s*[@:.\-]?\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
+            if not tp_match:
+                tp_match = re.findall(
+                    r'tp\s*(?:\d*\s*:\s*)?(\d+\.\d+)', sentence, re.IGNORECASE)
             if not tp_match:
                 tp_match = re.findall(
                     r'tp\s*:\s*(\d+\.?\d*)', sentence, re.IGNORECASE)
@@ -126,7 +140,7 @@ def GetTakeProfits(message):
                     r'takeprofit\s*1\s*=\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
             if not tp_match:
                 tp_match = re.findall(
-                    r'take\s*profit\s*1\s*:\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)      
+                    r'take\s*profit\s*1\s*:\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
             # if not tp_match:
             #     tp_match = re.findall(
             #         r'tp\s*(?:\d*[\s:]+)?(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
@@ -135,31 +149,34 @@ def GetTakeProfits(message):
             if tp_match:
                 tp_numbers.extend([float(tp) for tp in tp_match])
             else:
-                tp_matches = re.findall(r'tp\s*\d*\s*[@:.\-]?\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
+                tp_matches = re.findall(
+                    r'tp\s*\d*\s*[@:.\-]?\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
                 if tp_matches:
                     tp_numbers.extend([float(tp) for tp in tp_matches])
-            
-            tp_match_takeprofit = re.findall(r'take\s*profit\s*\d+\s*[-:]\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
+
+            tp_match_takeprofit = re.findall(
+                r'take\s*profit\s*\d+\s*[-:]\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
             if tp_match_takeprofit:
                 tp_numbers.extend([float(tp) for tp in tp_match_takeprofit])
 
             # Add TP2, TP3, TP4, etc., extraction logic
-            tp_match_2 = re.findall(r'tp(\d+)\s*[:\-]?\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
+            tp_match_2 = re.findall(
+                r'tp(\d+)\s*[:\-]?\s*(\d+\.\d+|\d+)', sentence, re.IGNORECASE)
             if tp_match_2:
                 for tp in tp_match_2:
                     tp_numbers.append(float(tp[1]))
-            
+
             # Check for comma-separated TP values in Persian
             persian_tp_match = re.findall(r'تی پی\s*([\d\s,،]+)', sentence)
             if persian_tp_match:
                 persian_tp_numbers = []
                 for match in persian_tp_match:
                     persian_tp_numbers.extend(
-                        [float(tp.strip()) for tp in re.split(r'[,\s،]+', match) if tp.strip().isdigit() and '/' not in tp]
+                        [float(tp.strip()) for tp in re.split(
+                            r'[,\s،]+', match) if tp.strip().isdigit() and '/' not in tp]
                     )
                 return persian_tp_numbers
 
-        
         if len(tp_numbers) == 0 or tp_numbers == 1.0:
             return None
         tp_numbers = set(tp_numbers)
@@ -181,7 +198,7 @@ def GetStopLoss(message):
                                  sentence, re.IGNORECASE)
             if not sl_match:
                 sl_match = re.search(
-                    r'sl\s*:\s*(\d+\.?\d*)', sentence, re.IGNORECASE)            
+                    r'sl\s*:\s*(\d+\.?\d*)', sentence, re.IGNORECASE)
             if not sl_match:
                 sl_match = re.search(r'(?i)stop\s*(\d+\.?\d*)', sentence)
             if not sl_match:
@@ -207,18 +224,21 @@ def GetStopLoss(message):
                     r'stop\s*loss\s*[@:]\s*(\d+\.?\d*)', message, re.IGNORECASE)
             if not sl_match:
                 sl_match = re.search(
-                    r'Stoploss\s*=\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)  
+                    r'Stoploss\s*=\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
             if not sl_match:
-                sl_match = re.search(r'SL\s*@\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)   
+                sl_match = re.search(
+                    r'SL\s*@\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
             if not sl_match:
-                sl_match = re.search(r'(?i)stop\s*loss\s*(\d+)', message) 
+                sl_match = re.search(r'(?i)stop\s*loss\s*(\d+)', message)
             if not sl_match:
                 sl_match = re.search(
                     r'استاپ\s*(\d+\.?\d*)', message, re.IGNORECASE)
             if not sl_match:
-                sl_match = re.search(r'sl[\s.:]*([\d]+\.?\d*)', sentence, re.IGNORECASE)
+                sl_match = re.search(
+                    r'sl[\s.:]*([\d]+\.?\d*)', sentence, re.IGNORECASE)
             if not sl_match:
-                sl_match = re.search(r'stop\s*loss\s*(?:point)?\s*[:\-]?\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
+                sl_match = re.search(
+                    r'stop\s*loss\s*(?:point)?\s*[:\-]?\s*(\d+\.\d+|\d+)', message, re.IGNORECASE)
             if sl_match:
                 sl_numbers.append(float(sl_match.group(1)))
             if not sl_numbers:
@@ -260,8 +280,9 @@ class TradeType(Enum):
 
 
 def read_symbol_list():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-    json_file_path = os.path.join(root_dir, "data", "Symbols.json");
+    root_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..', '..'))
+    json_file_path = os.path.join(root_dir, "data", "Symbols.json")
     try:
         with open(json_file_path, 'r') as file:
             data = json.load(file)
@@ -271,12 +292,14 @@ def read_symbol_list():
             "An error occurred while reading the symbol list JSON file")
         return []
 
+
 def find_similar_word(word, symbol_list):
     word_upper = word.upper()
     for symbol in symbol_list:
         if word_upper in symbol:
             return symbol  # Return the first match
     return None  # Return None if no match is found
+
 
 def GetSymbol(sentence):
     symbol_list = MetaTrader.GetSymbols()
@@ -299,11 +322,11 @@ def GetSymbol(sentence):
                 word == 'اونس'):
             return find_similar_word('XAUUSD', symbol_list)
         if word.upper() == "US30" or word == "داوجونز":
-            return find_similar_word("DJIUSD"  , symbol_list)    
+            return find_similar_word("DJIUSD", symbol_list)
         if word.upper() == "یورو":
             return find_similar_word("EURUSD", symbol_list)
         if word.upper() == "NASDAQ":
-            return find_similar_word("NDAQ", symbol_list)    
+            return find_similar_word("NDAQ", symbol_list)
         if word.upper() == "OIL":
             return find_similar_word("USO", symbol_list)
 
